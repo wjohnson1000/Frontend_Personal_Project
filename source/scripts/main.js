93172da176926ca115dc3cdca2101c186dc4ee16
William Johnson <wjohnson1000@gmail.com>
2015-11-13 15:08:50 -0700

    uploaded file structure and documentation, content lists etc

---
+++ source/scripts/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+// if(jQuery){
+//   alert('loaded');
+// }
+
+
+// var alchemyAPIquery = "https://gateway-a.watsonplatform.net/calls/data/GetNews?outputMode=json&start=now-1d&end=now&count=5&q.enriched.url.enrichedTitle.keywords.keyword.text=" + selectedTopic + "&return=enriched.url.url,enriched.url.title&apikey=884243d26352312be01ce6cfcfb5cf276e9000a2";
+
+// TWITTER API BELOW
+// // https://github.com/jublonet/codebird-js
+// var cb = new Codebird;
+// // Obtain a bearer token here: https://github.com/jublonet/codebird-js#application-only-auth
+// cb.setBearerToken("your bearer token here");
+// ​
+// // If you comment out this line, it will use the default proxy created by the codebird author https://api.jublo.net/codebird/
+// cb.setProxy('https://codebird-proxy.herokuapp.com/');
+// ​
+// //https://github.com/jublonet/codebird-js#requests-with-app-only-auth
+// //https://github.com/jublonet/codebird-js#mapping-api-methods-to-codebird-function-calls
+// cb.__call(
+//     "statuses_userTimeline",
+//     "screen_name=hillaryclinton",
+//     function (reply, rate, err) {
+//         console.log(reply);
+//     },
+//     true // this parameter required
+// );
+})
